4a674916e7875b461635dc6b5a48595399898ec9
Joan Carles <57914114+leibaral@users.noreply.github.com>
2023-06-06 03:09:08 +0200

    Delete S102 directory

--- S102/src/main/java/N1E1/VendaBuidaException.java
+++
@@ -1,6 +0,0 @@
-package N1E1;
-public class VendaBuidaException extends Exception {
-    public VendaBuidaException(String missatge) {
-        super(missatge);
-    }
-}
--- S102/src/main/java/N2E1/ErrorEntradaNoCompleix.java
+++
@@ -1,6 +0,0 @@
-package N2E1;
-public class ErrorEntradaNoCompleix extends Exception{
-    public ErrorEntradaNoCompleix(String message){
-        super(message);
-    }
-}
